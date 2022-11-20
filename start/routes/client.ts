e879ecfa8de1d385013d97571f905a8d197b1b62
RaffeuL <rafael91.lisboa@hotmail.com>
2022-11-20 15:27:45 -0300

    remove some routes

--- start/routes/client.ts
+++ start/routes/client.ts
@@ -1,5 +1,3 @@
 import Route from "@ioc:Adonis/Core/Route";
 
-Route.get("clients", "ClientsController.index");
-Route.post("clients", "ClientsController.store");
 Route.get("accounts", "AccountsController.index");
